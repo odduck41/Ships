d6c0e88194ee467821e8266f243aa92dcd82e678
leo <gd.cats.creators@gmail.com>
2026-03-19 10:10:32 +0300

    no progress

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include "lodepng/lodepng.h"
+#include <iostream>
+#include "pixel.h"
 
 int main() {
+    std::vector<unsigned char> out;
+    unsigned w, h;
+    const auto error =
+        lodepng::decode(out, w, h, std::string("../BW.png"), LCT_RGB);
+    if (error) {
+        std::cerr << error << std::endl;
+        return 0;
+    }
+
+    std::vector<std::vector<Color>> pixels;
 
 }
---
+++ pixel.h
@@ -0,0 +1,10 @@
+#ifndef SHIPS_PIXEL_H
+#define SHIPS_PIXEL_H
+
+struct Color {
+    unsigned char R{255};
+    unsigned char G{255};
+    unsigned char B{255};
+};
+
+#endif //SHIPS_PIXEL_H
